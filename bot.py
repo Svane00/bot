83a721a16e79f4a506aac6b0c763af0c30853508
Projectbot1 <64796260+Projectbot1@users.noreply.github.com>
2020-05-04 22:18:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,8 +32,12 @@ async def _8ball(ctx, *, question):
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def delete(ctx, amount=6):
+	await ctx.send(f'Deleting {amount} messages by the count of 3.')
+	await ctx.send('3')
+	await ctx.send('2')
+	await ctx.send('1')
     await ctx.channel.purge(limit=amount)
-    await ctx.send(f'I deleted `{amount} Messages.`')
+    await ctx.send(f'I deleted `{amount+4}` Messages.`')
 
 @client.command()
 @commands.has_permissions(manage_messages=True)
